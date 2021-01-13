c54c3d1ae674944c6ac6e666483de92cfc96cf98
David Granqvist <david.granqvist@stratsys.se>
2021-01-13 09:45:20 +0100

    Update Dockerfile * export tar artifact archive correctly again

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-DOCKER_BUILDKIT=1 docker build --rm -t windows_exporter:build --build-arg VERSION_TAG="$VERSION_TAG" . && (docker run --rm windows_exporter:build sh | tar xvf -)
+DOCKER_BUILDKIT=1 docker build --rm -t windows_exporter:build --build-arg VERSION_TAG="$VERSION_TAG" . && (docker run --rm windows_exporter:build | tar xvf -)
